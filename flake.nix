2ffe021f1114cc347eb1ce06b8f73a73d259b5ae
Quinn <quinn.schubert@icloud.com>
2025-12-27 13:48:07 +0100

    removed home-manager

--- flake.nix
+++ flake.nix
@@ -4,10 +4,6 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
@@ -16,7 +12,6 @@
         specialArgs = { inherit inputs; };
         modules = [
           ./hosts/main/configuration.nix
-          inputs.home-manager.nixosModules.default
         ];
       };
     };
