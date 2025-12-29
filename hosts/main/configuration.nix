0f5bec728eb94e676194cc033d31dde12c03c9f0
Qewa2 <quinn.schubert@icloud.com>
2025-12-29 23:58:59 +0100

    fixed steam

--- hosts/main/configuration.nix
+++ hosts/main/configuration.nix
@@ -110,6 +110,12 @@
      fzf
      inputs.awww.packages.${pkgs.stdenv.hostPlatform.system}.awww
   ];
+  programs.steam = {
+  	enable = true;
+	remotePlay.openFirewall = true;
+	dedicatedServer.openFirewall = true;
+	localNetworkGameTransfers.openFirewall = true;
+  };
   programs.hyprland.enable = true;
   services.pipewire.enable = false;
   services.pulseaudio.enable = true;
