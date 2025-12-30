e23b3b74a7726c24f83a3c363405dfb53a9c0c6c
Qewa2 <quinn.schubert@icloud.com>
2025-12-30 18:49:17 +0100

    fixed indent width
    Signed-off-by: Qewa2 <quinn.schubert@icloud.com>

--- flake.nix
+++ flake.nix
@@ -1,32 +1,32 @@
 {
-  description = "Nixos config flake";
+        description = "Nixos config flake";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    
-    awww.url = "git+https://codeberg.org/LGFae/awww";
-    nvf  = {
-      url = "github:NotAShelf/nvf";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-  };
+        inputs = {
+                nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs, nvf, home-manager, ... }@inputs: {
-    nixosConfigurations = {
-      main = nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit inputs; };
-        system = "x86_64-linux";
-	modules = [
-          ./hosts/main/configuration.nix
-	  home-manager.nixosModules.home-manager
-	  nvf.nixosModules.default
-        ];
-      };
-    };
-  };
+                home-manager = {
+                        url = "github:nix-community/home-manager";
+                        inputs.nixpkgs.follows = "nixpkgs";
+                };
+
+                awww.url = "git+https://codeberg.org/LGFae/awww";
+                nvf  = {
+                        url = "github:NotAShelf/nvf";
+                        inputs.nixpkgs.follows = "nixpkgs";
+                };
+        };
+
+        outputs = { self, nixpkgs, nvf, home-manager, ... }@inputs: {
+                nixosConfigurations = {
+                        main = nixpkgs.lib.nixosSystem {
+                                specialArgs = { inherit inputs; };
+                                system = "x86_64-linux";
+                                modules = [
+                                        ./hosts/main/configuration.nix
+                                        home-manager.nixosModules.home-manager
+                                        nvf.nixosModules.default
+                                ];
+                        };
+                };
+        };
 }
--- hosts/main/configuration.nix
+++ hosts/main/configuration.nix
@@ -5,193 +5,202 @@
 { config, pkgs, inputs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-      inputs.home-manager.nixosModules.default
-      ../../modules/qConf/qConf.nix
-    ];
+        imports =
+        [ # Include the results of the hardware scan.
+                ./hardware-configuration.nix
+                inputs.home-manager.nixosModules.default
+                ../../modules/qConf/qConf.nix
+        ];
   
-  qConf.enable = true;
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+        qConf.enable = true;
+        networking.hostName = "nixos"; # Define your hostname.
+        # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+        # Configure network proxy if necessary
+        # networking.proxy.default = "http://user:password@proxy:port/";
+        # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # Enable networking
-  networking.networkmanager.enable = true;
+        # Enable networking
+        networking.networkmanager.enable = true;
   
-  system.activationScripts.NixOsPermissions = {
-    text = ''
-      find /etc/nixos -type f -exec chown root:nixos {} +
-      find /etc/nixos -type f -exec chmod -R 664 {} +
-      chmod -R 774 /etc/nixos/modules/qConf/boot.sh
-    '';
-  };
+        system.activationScripts.NixOsPermissions = {
+                text = ''
+                        find /etc/nixos -type f -exec chown root:nixos {} +
+                        find /etc/nixos -type f -exec chmod -R 664 {} +
+                        chmod -R 774 /etc/nixos/modules/qConf/boot.sh
+                '';
+        };
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+        nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  # Set your time zone.
-  time.timeZone = "Europe/Berlin";
+        # Set your time zone.
+        time.timeZone = "Europe/Berlin";
 
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
+        # Select internationalisation properties.
+        i18n.defaultLocale = "en_US.UTF-8";
 
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "de_DE.UTF-8";
-    LC_IDENTIFICATION = "de_DE.UTF-8";
-    LC_MEASUREMENT = "de_DE.UTF-8";
-    LC_MONETARY = "de_DE.UTF-8";
-    LC_NAME = "de_DE.UTF-8";
-    LC_NUMERIC = "de_DE.UTF-8";
-    LC_PAPER = "de_DE.UTF-8";
-    LC_TELEPHONE = "de_DE.UTF-8";
-    LC_TIME = "de_DE.UTF-8";
-  };
+        i18n.extraLocaleSettings = {
+                LC_ADDRESS = "de_DE.UTF-8";
+                LC_IDENTIFICATION = "de_DE.UTF-8";
+                LC_MEASUREMENT = "de_DE.UTF-8";
+                LC_MONETARY = "de_DE.UTF-8";
+                LC_NAME = "de_DE.UTF-8";
+                LC_NUMERIC = "de_DE.UTF-8";
+                LC_PAPER = "de_DE.UTF-8";
+                LC_TELEPHONE = "de_DE.UTF-8";
+                LC_TIME = "de_DE.UTF-8";
+        };
 
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "de";
-    variant = "";
-  };
+        # Configure keymap in X11
+        services.xserver.xkb = {
+                layout = "de";
+                variant = "";
+        };
 
-  # Configure console keymap
-  console.keyMap = "de";
+        # Configure console keymap
+        console.keyMap = "de";
 
-  # Create the nixos group
-  users.groups.nixos = {
-    gid = 991;
-  };
+        # Create the nixos group
+        users.groups.nixos = {
+                gid = 991;
+        };
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.qewa = {
-    isNormalUser = true;
-    description = "Qewa";
-    shell = pkgs.bash;
-    extraGroups = [ "networkmanager" "wheel" "nixos" ];
-    packages = with pkgs; [];
-  };
+        # Define a user account. Don't forget to set a password with ‘passwd’.
+        users.users.qewa = {
+                isNormalUser = true;
+                description = "Qewa";
+                shell = pkgs.bash;
+                extraGroups = [ "networkmanager" "wheel" "nixos" ];
+                packages = with pkgs; [];
+        };
 
-  home-manager = {
-    extraSpecialArgs = { inherit inputs; };
-    useGlobalPkgs = true;
-    useUserPackages = true;
-    users = {
-      qewa = ./home.nix;
-    };
-  };
+        home-manager = {
+                extraSpecialArgs = { inherit inputs; };
+                useGlobalPkgs = true;
+                useUserPackages = true;
+                users = {
+                        qewa = ./home.nix;
+                };
+        };
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-  nixpkgs.config.allowUnfreePredicate = (_: true);
+        # Allow unfree packages
+        nixpkgs.config.allowUnfree = true;
+        nixpkgs.config.allowUnfreePredicate = (_: true);
 
-  # Change ownership of /etc/nixos to the nixos group
-  systemd.tmpfiles.rules = [
-    "d /etc/nixos 0775 root nixos -"
-  ];
+        # Change ownership of /etc/nixos to the nixos group
+        systemd.tmpfiles.rules = [
+                "d /etc/nixos 0775 root nixos -"
+        ];
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
-     vim
-     neofetch
-     fastfetch
-     gitFull
-     hyprland
-     kitty
-     brave
-     ffmpeg
-     wl-clipboard
-     wofi
-     pulseaudio
-     pavucontrol
-     tree
-     bash
-     sudo
-     hyprcursor
-     bibata-cursors
-     lshw
-     fzf
-     inputs.awww.packages.${pkgs.stdenv.hostPlatform.system}.awww
-  ];
+        # List packages installed in system profile. To search, run:
+        # $ nix search wget
+        environment.systemPackages = with pkgs; [
+                vim
+                neofetch
+                fastfetch
+                gitFull
+                hyprland
+                kitty
+                brave
+                ffmpeg
+                wl-clipboard
+                wofi
+                pulseaudio
+                pavucontrol
+                tree
+                bash
+                sudo
+                hyprcursor
+                bibata-cursors
+                lshw
+                fzf
+                inputs.awww.packages.${pkgs.stdenv.hostPlatform.system}.awww
+        ];
 
-  programs.nvf = {
-    enable = true;
-    settings.vim = {
-      theme = {
-        enable = true;
-        name = "gruvbox";
-	style = "dark";
-      };
-    statusline.lualine.enable = true;
-    telescope.enable = true;
-    autocomplete.nvim-cmp.enable = true;
-    };
-  };
+        programs.nvf = {
+                enable = true;
+                settings.vim = {
+                        theme = {
+                                enable = true;
+                                name = "gruvbox";
+                                style = "dark";
+                        };
+      
+                        statusline.lualine.enable = true;
+                        telescope.enable = true;
+                        autocomplete.nvim-cmp.enable = true;
+                        
+                        lsp.enable = true;
+                        languages = {
+                                enableTreesitter = true;
+                                nix.enable = true;
+                                clang.enable = true;
+                                python.enable = true;
+                        };
+                };
+        };
 
-  programs.steam = {
-  	enable = true;
-	remotePlay.openFirewall = true;
-	dedicatedServer.openFirewall = true;
-	localNetworkGameTransfers.openFirewall = true;
-  };
-  programs.hyprland.enable = true;
-  services.pipewire.enable = false;
-  services.pulseaudio.enable = true;
-  services.pulseaudio.support32Bit = true;
+        programs.steam = {
+                enable = true;
+                remotePlay.openFirewall = true;
+                dedicatedServer.openFirewall = true;
+                localNetworkGameTransfers.openFirewall = true;
+        };
   
-  # Nvidia
-  hardware.graphics.enable = true;
-  services.xserver.videoDrivers = ["nvidia"];
-  hardware.nvidia = {
-    modesetting.enable = true;
-    powerManagement.enable = false;
-    powerManagement.finegrained = false;
-    open = false;
-    nvidiaSettings = true;
-    prime = {
-      sync.enable = true;
-      intelBusId = "PCI:0:2:0";
-      nvidiaBusId = "PCI:1:0:0";
-    };
-  };
+        programs.hyprland.enable = true;
 
-  environment.variables = {
-    EDITOR = "nvim";
-    HYPRCURSOR_THEME = "Bibata-Modern-Classic";
-    HYPRCURSOR_SIZE = 24;
-    XCURSOR_THEME = "Bibata-Modern-Classic";
-    XCURSOR_SIZE = 24;
-  };
+        services.pipewire.enable = false;
+        services.pulseaudio.enable = true;
+        services.pulseaudio.support32Bit = true;
+  
+        # Nvidia
+        hardware.graphics.enable = true;
+        services.xserver.videoDrivers = ["nvidia"];
+        hardware.nvidia = {
+                modesetting.enable = true;
+                powerManagement.enable = false;
+                powerManagement.finegrained = false;
+                open = false;
+                nvidiaSettings = true;
+                prime = {
+                        sync.enable = true;
+                        intelBusId = "PCI:0:2:0";
+                        nvidiaBusId = "PCI:1:0:0";
+                };
+        };
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  programs.mtr.enable = true;
-  programs.gnupg.agent = {
-    enable = true;
-    enableSSHSupport = true;
-  };
+        environment.variables = {
+                EDITOR = "nvim";
+                HYPRCURSOR_THEME = "Bibata-Modern-Classic";
+                HYPRCURSOR_SIZE = 24;
+                XCURSOR_THEME = "Bibata-Modern-Classic";
+                XCURSOR_SIZE = 24;
+        };
 
-  # List services that you want to enable:
+        # Some programs need SUID wrappers, can be configured further or are
+        # started in user sessions.
+        programs.mtr.enable = true;
+        programs.gnupg.agent = {
+                enable = true;
+                enableSSHSupport = true;
+        };
 
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
+        # List services that you want to enable:
 
-  # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [ 22 ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+        # Enable the OpenSSH daemon.
+        services.openssh.enable = true;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.11"; # Did you read the comment?
+        # Open ports in the firewall.
+        networking.firewall.allowedTCPPorts = [ 22 ];
+        # networking.firewall.allowedUDPPorts = [ ... ];
+        # Or disable the firewall altogether.
+        # networking.firewall.enable = false;
+
+        # This value determines the NixOS release from which the default
+        # settings for stateful data, like file locations and database versions
+        # on your system were taken. It‘s perfectly fine and recommended to leave
+        # this value at the release version of the first install of this system.
+        # Before changing this value read the documentation for this option
+        # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+        system.stateVersion = "25.11"; # Did you read the comment?
 }
