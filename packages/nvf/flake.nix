3c12289aa1bf369a1c42cd285a641ce40b017ba2
Qewa2 <quinn.schubert@icloud.com>
2026-01-01 23:55:46 +0100

    tried to use a basic config (doesn't work)
    Signed-off-by: Qewa2 <quinn.schubert@icloud.com>

---
+++ packages/nvf/flake.nix
@@ -0,0 +1,20 @@
+{
+        inputs = {
+                nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+                nvf.url = "github:NotAShelf/nvf";
+        };
+
+        outputs = { self, nixpkgs, nvf, ... }: {
+                packages."x86_64-linux".default =
+                        (nvf.lib.neovimConfiguration {
+                                pkgs = nixpkgs.legacyPackages."x86_64-linux";
+                                modules = [ ./configuration.nix ];
+                        }).neovim;
+                
+                nixosConfiguration.nixos = nixpkgs.lib.nixosSystem {
+                        modules = [
+                                nvf.nixosModules.default
+                        ];
+                };
+        };
+}
