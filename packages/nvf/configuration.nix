debb4f8ea690ebf62050e341e212ae82514549da
Qewa2 <quinn.schubert@icloud.com>
2026-01-05 20:44:46 +0100

    added other configs from hosts/main/configuration.nix
    Signed-off-by: Qewa2 <quinn.schubert@icloud.com>

--- packages/nvf/configuration.nix
+++ packages/nvf/configuration.nix
@@ -18,7 +18,11 @@
                         style = "dark";
                         transparent = false;
                 };
-		
+
+                statusline.lualine.enable = true;
+                telescope.enable = true;
+                autocomplete.nvim-cmp.enable = true;
+
                 lsp.enable = true;
                 languages = {
 			enableTreesitter = true;
