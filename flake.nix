d85fb90e4b0fbaa62bc00537fb6deacc387e8686
Qewa2 <151440446+Qewa2@users.noreply.github.com>
2026-01-14 14:04:05 +0100

    Add tailsNix configuration to flake.nix

--- flake.nix
+++ flake.nix
@@ -30,6 +30,13 @@
                                         nvf.nixosModules.default
                                 ];
                         };
+                        tailsNix = nixpkgs.lib.nixosSystem {
+                                specialArgs = { inherit inputs; };
+				system = "x86_64-linux";
+				modules = [
+					./hosts/tailsNix/configuration.nix
+				];
+                        };
                 };
         };
 }
