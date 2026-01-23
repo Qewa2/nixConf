064dadbc053307aa3e2e02332e4a6aaf02adb13c
Qewa2 <quinn.schubert@icloud.com>
2026-01-23 23:11:07 +0100

    added mango config and dotfile module
    Signed-off-by: Qewa2 <quinn.schubert@icloud.com>

--- modules/dotfiles/dotfiles.nix
+++ modules/dotfiles/dotfiles.nix
@@ -1,9 +1,48 @@
-{ config, ... }:
+{ config, lib, ... }:
 let
         dotfiles = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/modules/dotfiles";
+        cfg = config.dotfiles;
+
+        niriEnable = cfg.niri.enable || cfg.enableAll;
+        mangoEnable = cfg.mango.enable || cfg.enableAll;
+        moduleEnable = cfg.enable || cfg.enableAll;
 in
 {
-        environment.etc = {
-                "niri/config.kdl".source = "${dotfiles}/niri/config.kdl";
+        options.dotfiles = {
+                enable = lib.mkOption {
+                        type = lib.types.bool;
+                        default = false;
+                };
+                
+                enableAll = lib.mkOption {
+                        type = lib.types.bool;
+                        default = false;
+                };
+
+                niri.enable = lib.mkOption {
+                        type = lib.types.bool;
+                        default = false;
+                };
+                
+                mango.enable = lib.mkOption {
+                        type = lib.types.bool;
+                        default = false;
+                };
         };
+        
+        config = lib.mkIf moduleEnable (lib.mkMerge [
+                (lib.mkIf niriEnable {
+                        environment.etc = {
+                                "niri".source = "${dotfiles}/niri";
+                        };
+                        programs.niri.enable = lib.mkDefault true;
+                })
+
+                (lib.mkIf mangoEnable {
+                        environment.etc = {
+                                "mango".source = "${dotfiles}/mango";
+                        };
+                        programs.mango.enable = lib.mkDefault true;
+                })
+        ]);
 }
