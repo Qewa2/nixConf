b6f6f02371c5889fdf14a04f3f1de8a1278ce891
Qewa2 <quinn.schubert@icloud.com>
2026-02-23 18:46:15 +0100

    tried a fix
    Signed-off-by: Qewa2 <quinn.schubert@icloud.com>

--- modules/nixosModules/Machines/users/qewa.nix
+++ modules/nixosModules/Machines/users/qewa.nix
@@ -1,5 +1,5 @@
-{ inputs, ... }: {
-        flake.nixosModules.users.qewa = { pkgs, ... }: {
+{
+        flake.nixosModules.users.qewa = { inputs, pkgs, ... }: {
                 imports = [
                         inputs.home-manager.nixosModules.default
                 ];
