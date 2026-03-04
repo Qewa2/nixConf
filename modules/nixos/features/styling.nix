35a1da46c04680b4680e2ed0f5f82832a6c4efa8
Qewa2 <quinn.schubert@icloud.com>
2026-03-04 20:57:46 +0100

    set the default theme for stylix

--- modules/nixos/features/styling.nix
+++ modules/nixos/features/styling.nix
@@ -1,9 +1,23 @@
 {
-        flake.nixosModules.styling = { pkgs, lib, ... }: {
+        flake.nixosModules.styling = { pkgs, lib, config, ... }: {
                  stylix = {
                         enable = true;
                         autoEnable = lib.mkDefault true;
                         base16Scheme = lib.mkDefault"${pkgs.base16-schemes}/share/themes/gruvbox-dark-soft.yaml";
+                        fonts = {
+                                monospace = lib.mkDefault {
+                                        package = pkgs.nerd-fonts.fira-code;
+                                        name = "FiraCode Nerd Font Mono";
+                                };
+
+                                emoji = lib.mkDefault {
+                                        package = pkgs.noto-fonts-color-emoji;
+                                        name = "Noto Color Emoji";
+                                };
+
+                                serif = lib.mkDefault config.stylix.fonts.monospace;
+                                sansSerif = lib.mkDefault config.stylix.fonts.monospace;
+                        };
                 };
         };
 }
