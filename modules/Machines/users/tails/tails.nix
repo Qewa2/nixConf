b9316f81b8e397bf2984f5db3a6baa54abea6d86
Qewa2 <quinn.schubert@icloud.com>
2026-03-16 19:15:01 +0100

    added tails user

---
+++ modules/Machines/users/tails/tails.nix
@@ -0,0 +1,36 @@
+{ self, ... }: {
+        flake.nixosModules.tails = { pkgs, ... }: {
+                users.users.tails = {
+                        isNormalUser = true;
+                        description = "Tails";
+                        shell = pkgs.bash;
+                        extraGroups = [ "networkmanager" "audio" "wheel" "nixos" ];
+                };
+
+                home-manager.users.qewa = {
+                        imports = [
+                                self.homeModules.base
+
+                                self.homeModules.btop
+
+                                self.homeModules.styling
+                        ];
+
+                        home = {
+                                username = "tails";
+                                homeDirectory = "/home/tails";
+                                stateVersion = "25.11";
+
+                                file = {
+                                        ".config/mango".source = ../qewa/mango;
+                                };
+                        };
+
+                        programs = {
+                                kitty.enable = true;
+                                rofi.enable = true;
+                                yazi.enable = true;
+                        };
+                };
+        };
+}
