9505ab2f863cf94a3bfb78b98fe8872d43f76c52
Qewa2 <quinn.schubert@icloud.com>
2026-03-17 17:16:06 +0100

    added awww and start awww

--- modules/Machines/users/tails/tails.nix
+++ modules/Machines/users/tails/tails.nix
@@ -1,4 +1,4 @@
-{ self, ... }: {
+{ inputs, self, ... }: {
         flake.nixosModules.tails = { pkgs, ... }: {
                 users.users.tails = {
                         isNormalUser = true;
@@ -24,7 +24,22 @@
 
                                 file = {
                                         ".config/mango".source = ../qewa/mango;
+                                        "wallpaper.jpg".source = ./Gradient.jpg; # https://wallhaven.cc/w/70p7p0
                                 };
+
+                                packages = with pkgs; [
+                                        inputs.awww.packages.${pkgs.stdenv.hostPlatform.system}.awww
+                                ];
+                        };
+
+
+                        systemd.user.services.set-wallpaper = {
+                                Unit.Description = "Set wallpaper for awww";
+                                Service = {
+                                        Type = "oneshot";
+                                        ExecStart = "${pkgs.lib.getExe inputs.awww.packages.${pkgs.stdenv.hostPlatform.system}.awww} img /home/tails/wallpaper.jpg";
+                                };
+                                Install.WantedBy = [ "graphical-session.target" ];
                         };
 
                         programs = {
