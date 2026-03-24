580a6520d004437dbf3132d445bf4b246a653af5
Qewa2 <quinn.schubert@icloud.com>
2026-03-24 22:06:34 +0100

    added winit as a flake input

--- flake.nix
+++ flake.nix
@@ -4,6 +4,11 @@
         inputs = {
                 nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
 
+                winit = {
+                        url = "github:Qewa2/winit";
+                        inputs.nixpkgs.follows = "nixpkgs";
+                };
+
                 home-manager = {
                         url = "github:nix-community/home-manager";
                         inputs.nixpkgs.follows = "nixpkgs";
