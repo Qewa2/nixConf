e78bbd76526694915171f434e50f587decec94f9
Qewa2 <enthusiast-01bergkessel@icloud.com>
2026-04-12 14:48:50 +0200

    force config replacement and add coreutils as dependency

--- modules/perSystem/wrappedPrograms/otter-launcher/otter-launcher.nix
+++ modules/perSystem/wrappedPrograms/otter-launcher/otter-launcher.nix
@@ -5,35 +5,13 @@
                         package = inputs.otter-launcher.packages.${pkgs.stdenv.hostPlatform.system}.otter-launcher;
                         runtimeInputs = [
                                 self.packages.${pkgs.stdenv.hostPlatform.system}.fsel
+                                pkgs.coreutils
                         ];
 
                         preHook = ''
-                                export SHOULD_DELETE_DIR=true
-                                export SHOULD_DELETE_FILE=true
-
-                                if [ -d "$HOME/.config/otter-launcher/" ]; then
-                                        echo "otter configuration directory already exists"
-                                        SHOULD_DELETE_DIR=false
-                                fi
-
-                                if [ -f "$HOME/.config/otter-launcher/config.toml" ]; then
-                                        echo "otter configuration already exists"
-                                        SHOULD_DELETE_FILE=false
-                                        SHOULD_DELETE_DIR=false
-                                else
-                                        mkdir -p "$HOME/.config/otter-launcher/"
-                                        ln -s ${toString ./config.toml} "$HOME/.config/otter-launcher/config.toml"
-                                fi
-                        '';
-
-                        postHook = ''
-                                if [ $SHOULD_DELETE_FILE == true ]; then
-                                        rm -rf "$HOME/.config/otter-launcher/config.toml"
-                                fi
-
-                                if [ $SHOULD_DELETE_DIR == true ]; then
-                                        rm -rf "$HOME/.config/otter-launcher/"
-                                fi
+                                rm -rf "$HOME/.config/otter-launcher/"
+                                mkdir -p "$HOME/.config/otter-launcher/"
+                                ln -s ${toString ./config.toml} "$HOME/.config/otter-launcher/config.toml"
                         '';
                 };
         };
