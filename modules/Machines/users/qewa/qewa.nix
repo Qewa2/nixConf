571793ffd9f5d47b0d7ed2033cb115f273e9f438
Qewa2 <enthusiast-01bergkessel@icloud.com>
2026-04-25 14:04:11 +0200

    use custom freecad

--- modules/Machines/users/qewa/qewa.nix
+++ modules/Machines/users/qewa/qewa.nix
@@ -33,13 +33,13 @@
                                         ncdu
                                         brave
                                         wl-clipboard
-                                        freecad
                                         inputs.awww.packages.${pkgs.stdenv.hostPlatform.system}.awww
                                         self.packages.${pkgs.stdenv.hostPlatform.system}.fsel
                                         self.packages.${pkgs.stdenv.hostPlatform.system}.otter-launcher
                                         self.packages.${pkgs.stdenv.hostPlatform.system}.launcher
                                         self.packages.${pkgs.stdenv.hostPlatform.system}.mango
                                         self.packages.${pkgs.stdenv.hostPlatform.system}.blender
+                                        self.packages.${pkgs.stdenv.hostPlatform.system}.freecad
                                 ];
 
                                 file = {
